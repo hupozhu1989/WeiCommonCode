9a481de7eceb969f828a83a605705d54d5f112bc
weizhenchao <weizhenchao@richinfo.cn>
2020-03-19 13:19:29 +0800

    add:ThreadPoolExecutor

---
+++ src/main/java/com/wei/common/threadpoolexecutor/ThreadPoolConstants.java
@@ -0,0 +1,11 @@
+package com.wei.common.threadpoolexecutor;
+
+public class ThreadPoolConstants {
+    public static final int CORE_POOL_SIZE = 5;
+    public static final int MAX_POOL_SIZE = 10;
+    public static final int QUEUE_CAPACITY = 100;
+    public static final Long KEEP_ALIVE_TIME = 1L;
+    private ThreadPoolConstants(){
+        //私有构造方法,不允许创建对象实例,直接类名.静态变量
+    }
+}
