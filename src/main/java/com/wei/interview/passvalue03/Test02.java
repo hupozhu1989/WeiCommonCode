25ed4a0ad5a8a9fa4405d457d6cbcabaaeaa4f5e
hupozhu1989 <675320027@qq.com>
2020-03-27 23:21:19 +0800

    add:值传递

---
+++ src/main/java/com/wei/interview/passvalue03/Test02.java
@@ -0,0 +1,49 @@
+package com.wei.interview.passvalue03;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 栈管运行 堆管存储
+ * @author weizhenchao
+ * @version 1.0
+ * @date：2020/3/27
+ */
+public class Test02 {
+    public void changeValue1(int age){
+        age = 30;
+    }
+    public void changeValue2(com.wei.interview.passvalue03.Person person){
+        person.setPersonName("xxx");
+    }
+    public void changeValue3(String str){
+        str = "xxx";
+    }
+    public static void main(String[] args) {
+        Test02 test = new Test02();
+        int age = 20;
+        test.changeValue1(age);
+        System.out.println("age------"+age);
+
+        Person person = new Person("abc");
+        test.changeValue2(person);
+        System.out.println("personName------"+person.getPersonName());
+
+        String str = "abc";
+        test.changeValue3(str);
+        System.out.println("str------"+str);
+    }
+}
+
+@NoArgsConstructor
+@Getter
+@Setter
+class Person{
+    private Integer id;
+    private String personName;
+
+    public Person(String personName) {
+        this.personName = personName;
+    }
+}
