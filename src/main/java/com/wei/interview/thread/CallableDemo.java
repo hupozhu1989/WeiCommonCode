b3c805bbabdf7d68c04bc3944632612d198d7447
hupozhu1989 <675320027@qq.com>
2020-03-30 22:56:12 +0800

    add:thread v1.0

---
+++ src/main/java/com/wei/interview/thread/CallableDemo.java
@@ -0,0 +1,36 @@
+package com.wei.interview.thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @author weizhenchao
+ * @version 1.0
+ * @date：2020/3/30
+ */
+public class CallableDemo {
+    public static void main(String[] args) throws Exception{
+        FutureTask<Integer> futureTask = new FutureTask<>(new MyThread());
+        //FutureTask<Integer> futureTask2 = new FutureTask<>(new MyThread());
+        new Thread(futureTask,"AA").start();
+        //new Thread(futureTask2,"BB").start();
+        int result01 = 100;
+        System.out.println(Thread.currentThread().getName()+"\t");
+
+        /*while (futureTask.isDone()){
+
+        }*/
+        int result02 = futureTask.get();//一般get()方法放在最后面
+        System.out.println("******result:"+(result01+result02));
+    }
+
+
+}
+
+class MyThread implements Callable<Integer>{
+    @Override
+    public Integer call() throws Exception {
+        System.out.println(Thread.currentThread().getName()+"\tcome in");
+        return 1024;
+    }
+}
