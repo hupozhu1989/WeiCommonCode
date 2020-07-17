ccd2c643f962d4078e28cfe47a20cde1605efef3
hupozhu1989 <675320027@qq.com>
2020-07-17 23:19:27 +0800

    add:spring v1.3

---
+++ src/test/java/com/wei/spring/ioc/A.java
@@ -0,0 +1,18 @@
+package com.wei.spring.ioc;
+
+/**
+ * @author weizhenchao
+ * @version 1.0
+ * @date：2020/7/17
+ */
+public class A {
+    private B b;
+
+    public B getB() {
+        return b;
+    }
+
+    public void setB(B b) {
+        this.b = b;
+    }
+}
---
+++ src/test/java/com/wei/spring/ioc/B.java
@@ -0,0 +1,18 @@
+package com.wei.spring.ioc;
+
+/**
+ * @author weizhenchao
+ * @version 1.0
+ * @date：2020/7/17
+ */
+public class B {
+    private A a;
+
+    public A getA() {
+        return a;
+    }
+
+    public void setA(A a) {
+        this.a = a;
+    }
+}
