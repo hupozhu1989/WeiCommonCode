bda8b0b5d67d2fbc454657fa4359175883dc9d2b
weizc <weizhenchao@linshimuye.com>
2020-11-24 12:55:46 +0800

    add:hashmap遍历

---
+++ src/main/java/com/wei/common/collection/HashMapIterator.java
@@ -0,0 +1,160 @@
+package com.wei.common.collection;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * hashmap迭代
+ * 推荐使用 entrySet
+ * @author weizhenchao
+ * @create 2020-11-24-8:38
+ */
+public class HashMapIterator {
+    public static void main(String[] args) {
+        //迭代器 EntrySet
+        method01();
+        //迭代器 KeySet
+        method02();
+        //ForEach EntrySet
+        method03();
+        //ForEach KeySet
+        method04();
+        //Lambda
+        method05();
+        //Streams API 单线程
+        method06();
+        //Streams API 多线程
+        method07();
+    }
+
+    /**
+     * Streams API 多线程
+     */
+    private static void method07() {
+        // 创建并赋值 HashMap
+        Map<Integer, String> map = new HashMap();
+        map.put(1, "Java");
+        map.put(2, "JDK");
+        map.put(3, "Spring Framework");
+        map.put(4, "MyBatis framework");
+        map.put(5, "Java中文社群");
+        // 遍历
+        map.entrySet().parallelStream().forEach((entry) -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        });
+    }
+
+    /**
+     * Streams API 单线程
+     */
+    private static void method06() {
+        // 创建并赋值 HashMap
+        Map<Integer, String> map = new HashMap();
+        map.put(1, "Java");
+        map.put(2, "JDK");
+        map.put(3, "Spring Framework");
+        map.put(4, "MyBatis framework");
+        map.put(5, "Java中文社群");
+        // 遍历
+        map.entrySet().stream().forEach((entry) -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        });
+    }
+
+    /**
+     * Lambda
+     */
+    private static void method05() {
+        // 创建并赋值 HashMap
+        Map<Integer, String> map = new HashMap();
+        map.put(1, "Java");
+        map.put(2, "JDK");
+        map.put(3, "Spring Framework");
+        map.put(4, "MyBatis framework");
+        map.put(5, "Java中文社群");
+        // 遍历
+        map.forEach((key, value) -> {
+            System.out.println(key);
+            System.out.println(value);
+        });
+    }
+
+    /**
+     * ForEach KeySet
+     */
+    private static void method04() {
+        // 创建并赋值 HashMap
+        Map<Integer, String> map = new HashMap();
+        map.put(1, "Java");
+        map.put(2, "JDK");
+        map.put(3, "Spring Framework");
+        map.put(4, "MyBatis framework");
+        map.put(5, "Java中文社群");
+        // 遍历
+        for (Integer key : map.keySet()) {
+            System.out.println(key);
+            System.out.println(map.get(key));
+        }
+    }
+
+    /**
+     * ForEach EntrySet
+     */
+    private static void method03() {
+        // 创建并赋值 HashMap
+        Map<Integer, String> map = new HashMap();
+        map.put(1, "Java");
+        map.put(2, "JDK");
+        map.put(3, "Spring Framework");
+        map.put(4, "MyBatis framework");
+        map.put(5, "Java中文社群");
+        // 遍历
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+    }
+
+    /**
+     * 迭代器 KeySet
+     */
+    private static void method02() {
+        // 创建并赋值 HashMap
+        Map<Integer, String> map = new HashMap();
+        map.put(1, "Java");
+        map.put(2, "JDK");
+        map.put(3, "Spring Framework");
+        map.put(4, "MyBatis framework");
+        map.put(5, "Java中文社群");
+        // 遍历
+        Iterator<Integer> iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            Integer key = iterator.next();
+            System.out.println(key);
+            System.out.println(map.get(key));
+        }
+    }
+
+    /**
+     * 迭代器 EntrySet
+     */
+    private static void method01() {
+        // 创建并赋值 HashMap
+        Map<Integer, String> map = new HashMap();
+        map.put(1, "Java");
+        map.put(2, "JDK");
+        map.put(3, "Spring Framework");
+        map.put(4, "MyBatis framework");
+        map.put(5, "Java中文社群");
+        // 遍历
+        Iterator<Map.Entry<Integer, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, String> entry = iterator.next();
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+    }
+}
